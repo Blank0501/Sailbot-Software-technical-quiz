e2a2a282afcbce9801d4218da2b56edbe84709a2
Merrick Lu <merricklu@icloud.com>
2026-01-11 21:29:20 -0800

    Completed quiz and added unit tests

--- c-cpp/standard_calc.cpp
+++ c-cpp/standard_calc.cpp
@@ -13,7 +13,14 @@
  * @return float: The bounded angle in degrees.
  */
 float bound_to_180(float angle) {
-    return 0;
+	//Recursively adds or subtracts 360 until angle is within [-180, 180)
+    if(angle >= 180) {
+		return bound_to_180(angle - 360);
+	}
+	else if(angle < -180) {
+		return bound_to_180(angle + 360);
+	}
+    return angle;
 }
 
 /**
@@ -29,5 +36,14 @@ float bound_to_180(float angle) {
  * @return bool: TRUE when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, FALSE otherwise
  */
 bool is_angle_between(float first_angle, float middle_angle, float second_angle) {
-    return true;
+	float first_to_middle = bound_to_180(middle_angle - first_angle); //non-reflex angle between middle_angle and first_angle
+	float first_to_second = bound_to_180(second_angle - first_angle); //non-reflex angle between second_angle and first_angle
+
+	if(first_to_second > 0 && first_to_middle > 0) { //both angles are positive
+		return (first_to_second > first_to_middle);
+	}
+	else if(first_to_second < 0 && first_to_middle < 0) { //both angles are negative
+		return (first_to_second < first_to_middle);
+	}
+    return false;
 }
--- c-cpp/test_standard_calc.cpp
+++ c-cpp/test_standard_calc.cpp
@@ -5,11 +5,34 @@
 /**************** Tests for bound_to_180() *******************/
 void test_bound_basic1(CuTest *tc) {
     CuAssertDblEquals(tc, 0, bound_to_180(0), 0.0001);
+    CuAssertDblEquals(tc, 135, bound_to_180(135), 0.0001);
+    CuAssertDblEquals(tc, -160, bound_to_180(200), 0.0001);
+}
+
+void test_bound_edges(CuTest *tc) {
+    CuAssertDblEquals(tc, -180, bound_to_180(-180), 0.0001);
+    CuAssertDblEquals(tc, -180, bound_to_180(180), 0.0001);
+}
+
+void test_bound_wrapping(CuTest *tc) {
+    CuAssertDblEquals(tc, 0, bound_to_180(360), 0.0001);
+    CuAssertDblEquals(tc, -90, bound_to_180(-450), 0.0001);
+    CuAssertDblEquals(tc, -180, bound_to_180(-900), 0.0001);
 }
 
 /**************** Tests for is_angle_between() *******************/
 void test_between_basic1(CuTest *tc) {
     CuAssertTrue(tc, is_angle_between(0, 1, 2));
+    CuAssertTrue(tc, is_angle_between(350, 0, 10));
+    CuAssertTrue(tc, is_angle_between(90, 135, 180));
+    CuAssertTrue(tc, !is_angle_between(45, 90, 270));
+    CuAssertTrue(tc, !is_angle_between(0, 180, 90));
+    CuAssertTrue(tc, !is_angle_between(0, -90, 90));
+}
+void test_between_edges(CuTest *tc) {
+    CuAssertTrue(tc, !is_angle_between(0, 0, 90));
+    CuAssertTrue(tc, !is_angle_between(0, 90, 90));
+    CuAssertTrue(tc, !is_angle_between(10, 10, 10));
 }
 
 int main(int argc, char const *argv[]) {
@@ -17,7 +40,10 @@ int main(int argc, char const *argv[]) {
     CuSuite *suite = CuSuiteNew();
 
     SUITE_ADD_TEST(suite, test_bound_basic1);
+    SUITE_ADD_TEST(suite, test_bound_edges);
+    SUITE_ADD_TEST(suite, test_bound_wrapping);
     SUITE_ADD_TEST(suite, test_between_basic1);
+    SUITE_ADD_TEST(suite, test_between_edges);
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
